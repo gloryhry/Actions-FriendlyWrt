360fa696afb7d63521a21af6ae910ba5bc1fe3a8
GloryHRY <glory@hzbz.edu.eu.org>
2024-11-03 17:04:27 +0800

    add some pack

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -32,3 +32,93 @@ function init_theme() {
 EOL
 sed -i -e '/boardname=/r /tmp/appendtext.txt' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
 # }}
+
+# alist
+git clone https://github.com/sbwml/luci-app-alist package/alist
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+
+# Clone community packages
+mkdir package/community
+pushd package/community
+
+
+# Add Lienol's Packages
+git clone --depth=1 https://github.com/Lienol/openwrt-package
+rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
+rm -rf openwrt-package/verysync
+rm -rf openwrt-package/luci-app-verysync
+
+# Add luci-app-passwall
+git clone https://github.com/VictC79/openwrt-passwall
+git clone --depth=1 https://github.com/VictC79/openwrt-passwall2
+svn export https://github.com/VictC79/openwrt-passwall/trunk/luci-app-passwall
+           
+# Add luci-app-ssr-plus
+git clone --depth=1 https://github.com/fw876/helloworld
+
+# Add luci-app-unblockneteasemusic
+rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
+git clone --branch master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
+
+# Add luci-app-vssr <M>
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
+git clone --depth=1 https://github.com/VictC79/luci-app-vssr.git
+
+# Add luci-proto-minieap
+git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
+
+# Add OpenClash
+svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+
+# Add mihomo
+svn export https://github.com/morytyann/OpenWrt-mihomo/trunk/luci-app-mihomo
+
+# Add ddnsto & linkease
+svn export https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
+svn export https://github.com/linkease/nas-packages/trunk/network/services/ddnsto
+
+# Add luci-app-onliner (need luci-app-nlbwmon)
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
+
+# Add ServerChan
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+
+# Add luci-theme
+git clone https://github.com/DHDAXCW/theme
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+
+# Add subconverter
+git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
+
+# Add luci-app-smartdns & smartdns
+svn export https://github.com/VictC79/luci-app-smartdns/trunk/luci-app-smartdns
+
+# Add luci-app-services-wolplus
+svn export https://github.com/msylgj/OpenWrt_luci-app/trunk/luci-app-services-wolplus
+
+# Add apk (Apk Packages Manager)
+svn export https://github.com/openwrt/packages/trunk/utils/apk
+
+# Add luci-app-poweroff
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
+
+# Add OpenAppFilter
+git clone --depth=1 https://github.com/destan19/OpenAppFilter
+
+# Add luci-aliyundrive-webdav
+rm -rf ../../customfeeds/luci/applications/luci-app-aliyundrive-webdav
+rm -rf ../../customfeeds/packages/multimedia/aliyundrive-webdav
+svn export https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav
+svn export https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav
+popd
+
+
+# # Mod zzz-default-settings
+# pushd package/lean/default-settings/files
+# sed -i '/http/d' zzz-default-settings
+# sed -i '/18.06/d' zzz-default-settings
+# export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
+# sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
+# popd
